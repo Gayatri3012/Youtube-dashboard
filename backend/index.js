311b8cd53965827f0b158e29c0bc4d2518060986
Gayatri <takawalegayatri@gmail.com>
2025-08-17 14:20:30 +0530

    Add youtubeApiRequest util

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const {youtubeAPIRequest} = require("./utils/youtubeAPIRequest.js"); 
+const {youtubeAPIRequest} = require("./utils/youtubeApiRequest.js"); 
 
 const app = express();
 const allowedOrigins = [
